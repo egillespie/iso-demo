b020682c5701fbacce3d333725e40836c26e367f
Erik Gillespie <erik.gillespie@gmail.com>
2017-03-15 13:55:38 -0400

    Move project into GitHub

---
+++ README.md
@@ -0,0 +1,19 @@
+# Isometric Demo
+
+Demonstrates how to create an isometric room with a moveable character using
+no frameworks and minimal JavaScript. All of the work is in specially crafted
+wall images.
+
+You can view the demo at https://egillespie.github.io/iso-demo/
+
+## Usage
+
+Use the keyboard to move the character around and toggle opaque walls.
+
+| Key  |  Effect                        |
+|------|--------------------------------|
+| `j`  |  Move character up and left    |
+| `l`  |  Move character down and left  |
+| `i`  |  Move character up and right   |
+| `k`  |  Move character down and right |
+| `o`  |  Toggle opaque walls           |
---
+++ events.js
@@ -0,0 +1,68 @@
+var boy = document.getElementById('boy1')
+
+function onKeyPress (e) {
+  var keynum = window.event ? e.keyCode : e.which
+
+  switch (keynum) {
+    case 111: // o = see through walls
+      toggleOpacity()
+      break
+    case 106: // j = west
+      moveRel(boy.gridx-1, boy.gridy, -20, -10, -1)
+      break
+    case 105: // i = north
+      moveRel(boy.gridx, boy.gridy-1, 20, -10, -1)
+      break
+    case 108: // l = east
+      moveRel(boy.gridx+1, boy.gridy, 20, 10, 1)
+      break
+    case 107: // k = south
+      moveRel(boy.gridx, boy.gridy+1, -20, 10, 1)
+      break
+  }
+  return 0
+}
+
+function toggleOpacity () {
+  var elems = document.getElementsByTagName('div')
+
+  for (var i = 0; i < elems.length; i++) {
+    if (
+      elems[i].className.substring(0,4) == 'wall'
+      && elems[i].id.substring(0,2) != 'w1'
+      && elems[i].id.substring(3,4) != '0'
+    ) {
+      if (!elems[i].style.opacity || elems[i].style.opacity === 'none') {
+        elems[i].style.opacity = 0.4
+      } else {
+        elems[i].style.opacity = 1.0
+      }
+    }
+  }
+}
+
+function canMoveTo (x, y) {
+  return (document.getElementById('w' + y + '-' + x) ? false : true)
+}
+
+function moveRel (x, y, relx, rely, relz) {
+  if (canMoveTo(x, y)) {
+    boy.style.top = (parseInt(boy.style.top) + rely) + 'px'
+    boy.style.left = (parseInt(boy.style.left) + relx) + 'px'
+    boy.style.zIndex = parseInt(boy.style.zIndex) + relz
+    boy.gridx = x
+    boy.gridy = y
+  }
+}
+
+function init () {
+  boy.style.top = '112px'
+  boy.style.left = '205px'
+  boy.style.zIndex = 9
+  boy.gridx = 5
+  boy.gridy = 4
+
+  document.body.addEventListener('keypress', onKeyPress)
+}
+
+init()
