0d1e776f89a210e195a68e7f28e304577ef02118
Erik Gillespie <erik.gillespie@gmail.com>
2021-03-13 01:28:54 -0500

    Add floor tiles with JavaScript

--- src/scripts/events.js
+++ src/scripts/events.js
@@ -1,7 +1,26 @@
+const space = [
+  [null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null]
+]
+
+const container = document.getElementById('container')
 const player = document.getElementById('player')
 
-const PLAYER_X_ADJUST = 20
-const PLAYER_Y_ADJUST = 10
+const FLOOR_WIDTH = 40
+const FLOOR_HEIGHT = 30
+const FLOOR_LIFT = 10
+
+const WALL_WIDTH = 30
+const WALL_HEIGHT = 60
+const WALL_LIFT = 2
+
+const PLAYER_X_ADJUST = FLOOR_WIDTH / 2
+const PLAYER_Y_ADJUST = (FLOOR_HEIGHT - FLOOR_LIFT) / 2
 const PLAYER_Z_ADJUST = 1
 
 const WEST = {
@@ -74,12 +93,13 @@ function toggleOpacity () {
   const walls = document.getElementsByClassName('wall')
 
   for (const wall of walls) {
-    if (
-      wall.id.substring(0, 2) !== 'w1' &&
-      wall.id.substring(3, 4) !== '0'
-    ) {
-      wall.classList.toggle('translucent')
-    }
+    wall.classList.toggle('translucent')
+    // if (
+    //   wall.id.substring(0, 2) !== 'w1' &&
+    //   wall.id.substring(3, 4) !== '0'
+    // ) {
+    //   wall.classList.toggle('translucent')
+    // }
   }
 }
 
@@ -106,6 +126,52 @@ function moveCardinal (dir) {
     gridy: player.gridy + dir.gridyAdjust
   })
 }
+
+function insertSprite (node) {
+  container.insertBefore(node, player)
+}
+
+function floorTop (row, col) {
+  return 60 + ((row + col) * (FLOOR_HEIGHT - FLOOR_LIFT) / 2)
+}
+
+function floorLeft (row, col) {
+  const lastRow = space.length - 1
+  const rowLeft = 60 + ((lastRow - row) * FLOOR_WIDTH / 2)
+  return rowLeft + col * FLOOR_WIDTH / 2
+}
+
+function floorZIndex (row, col) {
+  return row + col
+}
+
+function createFloor (row, col) {
+  const floor = document.createElement('div')
+  floor.id = `f${row}-${col}`
+  floor.classList.add('floor')
+  floor.style.top = floorTop(row, col) + 'px'
+  floor.style.left = floorLeft(row, col) + 'px'
+  floor.style.zIndex = floorZIndex(row, col)
+  return floor
+}
+
+function createFloors () {
+  for (let row = 0; row < space.length; row++) {
+    for (let col = 0; col < space[row].length; col++) {
+      const floor = createFloor(row, col)
+      if (row === 1 && col === 0) {
+        console.log(floor)
+      }
+      insertSprite(floor)
+    }
+  }
+}
+
+function createRoom () {
+  createFloors()
+}
+
+createRoom()
 
 moveTo({
   top: 112,
