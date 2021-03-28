62e4f9967ea768b2c5f7b365b298afdc54595f8e
Erik Gillespie <erik.gillespie@gmail.com>
2021-03-28 14:55:56 -0400

    Move key handler to separate file

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -24,58 +24,10 @@ const room = mapToWallSegments(map)
 */
 
 const state = require('./state')
-
 const createFloors = require('./floors/floors-create')
 const createWalls = require('./walls/walls-create')
-
-const toggleOpacity = require('./walls/walls-opacity-toggle')
-const togglePlayerSprite = require('./player/player-sprite-toggle')
-
-const movePlayerNorth = require('./player/player-move-north')
-const movePlayerEast = require('./player/player-move-east')
-const movePlayerSouth = require('./player/player-move-south')
-const movePlayerWest = require('./player/player-move-west')
-
 const movePlayerTo = require('./player/player-move-to-position')
-
-function onKeyDown (event) {
-  switch (event.code) {
-    case 'KeyO':
-      // Toggle translucent/solid walls
-      event.preventDefault()
-      toggleOpacity()
-      break
-    case 'KeyP':
-      // Toggle player image
-      event.preventDefault()
-      togglePlayerSprite()
-      break
-    case 'KeyA':
-    case 'ArrowLeft':
-      // Move west (up and left)
-      event.preventDefault()
-      movePlayerWest(state.currentRoom)
-      break
-    case 'KeyW':
-    case 'ArrowUp':
-      // Move north (up and right)
-      event.preventDefault()
-      movePlayerNorth(state.currentRoom)
-      break
-    case 'KeyD':
-    case 'ArrowRight':
-      // Move east (down and right)
-      event.preventDefault()
-      movePlayerEast(state.currentRoom)
-      break
-    case 'KeyS':
-    case 'ArrowDown':
-      // Move south (down and left)
-      event.preventDefault()
-      movePlayerSouth(state.currentRoom)
-      break
-  }
-}
+const handleKeyDown = require('./events/handle-key-down')
 
 function createRoom () {
   const room = [
@@ -96,4 +48,4 @@ function createRoom () {
 
 createRoom()
 
-window.addEventListener('keydown', onKeyDown)
+window.addEventListener('keydown', handleKeyDown)
