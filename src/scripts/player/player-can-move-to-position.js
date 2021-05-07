60cd5f419bcf8fd8ef3959eea8a0aed131221bab
Erik Gillespie <erik.gillespie@gmail.com>
2021-05-06 21:22:39 -0400

    Prevent walking off bottom and right edges of current room

--- src/scripts/player/player-can-move-to-position.js
+++ src/scripts/player/player-can-move-to-position.js
@@ -1,9 +1,13 @@
+const state = require('../state')
+
 // Returns true if no walls exist in the specified position, allowing
 // the player to move there. Returns false otherwise.
 module.exports = function (row, col) {
   return (
     row >= 0 &&
     col >= 0 &&
+    row < state.currentRoom.length &&
+    col < state.currentRoom[row].length &&
     !document.getElementById(`w${row}-${col}`)
   )
 }
